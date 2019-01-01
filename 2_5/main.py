0cf60ea937981e8da929b40b0069fc4fdda0a794
Anton Ly <anton96lu@gmail.com>
2019-01-01 16:58:20 +0100

    2.5 kinda working

--- 2_5/main.py
+++ 2_5/main.py
@@ -1,7 +1,7 @@
 import pickle
 from ex_2_3 import Node, load_params, load_sample, print_tree
-from ex_2_3_tree_helper import Tree
-import sys
+from helper import Tree
+import numpy as np
 
 """
     The data is stored in Newick form:
@@ -46,10 +46,10 @@ import sys
 
 my_data_path = ''
 
-with open(my_data_path + 'tree_params.pickle', 'rb') as handle:
+with open(my_data_path + 'tree_with_CPD.pkl', 'rb') as handle:
     params = pickle.load(handle, encoding='latin1')
 
-with open(my_data_path + 'tree_samples.pickle', 'rb') as handle:
+with open(my_data_path + 'tree_with_leaf_samples.pkl', 'rb') as handle:
     samples = pickle.load(handle, encoding='latin1')
 
 
@@ -57,8 +57,7 @@ with open(my_data_path + 'tree_samples.pickle', 'rb') as handle:
 """
     Construct a tree with parameters from the loaded parameter dict.
 """
-params_name = list(params.keys())[0]     
-params = params[params_name]
+params_list = params
 root = load_params(params)
 
 
@@ -66,57 +65,57 @@ root = load_params(params)
 """
     Load a matching sample into the tree.
 """
-samples_name = params_name + '_sample_1'
-sample = samples[samples_name]
-load_sample(root, sample)
+load_sample(root, samples)
 
 
 
 """
 Print the tree (not very sophisticated). Structure: nodename_parentname
 """
-print_tree(root)
+# print_tree(root)
 
 """
 Print the tree with sample (not very sophisticated). Structure: nodename_parentname:sample
 """
 print_tree(root, print_sample = True)
 
-"""
-Use tree object:
-"""
+# """
+# Use tree object:
+# """
 
-print("\nTree")
+# print("\nTree")
 
-t = Tree()    
+# t = Tree()    
     
 
-my_data_path = ''
+# my_data_path = ''
 
-with open(my_data_path + 'tree_params.pickle', 'rb') as handle:
-    params = pickle.load(handle, encoding='latin1')
+# with open(my_data_path + 'tree_params.pickle', 'rb') as handle:
+#     params = pickle.load(handle, encoding='latin1')
 
-key = list(params.keys())[0]    
+# key = list(params.keys())[0]    
     
-"""
-Load params into tree
-"""
-t.load_params(params[key])
-t.print_tree()        
+# """
+# Load params into tree
+# """
+# t.load_params(params[key])
+# # t.print_tree()        
 
-"""
-Generate a random tree
-"""
-t.create_random_tree(3)
-t.print_tree() 
+# """
+# Generate a random tree
+# """
+# t.create_random_tree(3)
+# # t.print_tree() 
 
 print()
 
+####################################################################################################
+#
 # Own code 
-rt = t.root
+#
+####################################################################################################
 
-
-def rec(node, i) :
+def rec(node, i, s_dict) :
     decendants = node.descendants
     distr = node.cat
     # print("node sample: " + str(node.sample))
@@ -129,23 +128,143 @@ def rec(node, i) :
             return 0
 
     s = 1
+    if node in s_dict[i] :
+        s = s_dict[i][node]
+    else :
+        for dec in decendants :
 
-    for dec in decendants :
+            ssum = 0
 
-        ssum = 0
-        for j in range(0, 3) :
-            ssum += dec.cat[j][i] * rec(dec, j)
-        
-        s *= ssum
+            for j in range(0, len(dec.cat[i])) :
+                ssum += dec.cat[i][j] * rec(dec, j, s_dict)
+            
+            s *= ssum
+
+        s_dict[i][node] = s
 
     return s
+
+def compute_posterior(node, s_dict, p_dict) :
+    decendants = node.descendants
+    distr = node.cat
+
+    # Check if leaf
+    if decendants == [] :
+        return
+
+    for j in range(0, len(distr)) :
+        if node in p_dict[j] :
+            continue
+        else : # compute posterior
+            p = 0
+            s = s_dict[j][node]
+            for i in range(0, len(distr[j])) :
+                cat = distr[j][i] 
+                p += s * cat
+            p_dict[j][node] = p
+
+        for dec in decendants :
+            compute_posterior(dec, s_dict, p_dict)
+    return
+
+def normalize_posterior(node, p_dict) :
+    decendants = node.descendants
+    distr = node.cat
+
+    # Check if leaf
+    if decendants == [] :
+        return
+
+    # determine normalizer
+    denom = 0
+    for i in range(0, len(distr)) :
+        denom += p_dict[i][node]
+
+    # normalize
+    for i in range(0, len(distr)) :
+        p_dict[i][node] = p_dict[i][node] / denom
+
+    # normalize children
+    for dec in decendants :
+        normalize_posterior(dec, p_dict)
+
+def assemble_posterior(node, i, p_dict) :
+    decendants = node.descendants
+    distr = node.cat
+
+    if decendants == [] :
+        return 1
+
+    p = p_dict[i][node]
+    for dec in decendants :
+        p *= assemble_posterior(dec, i, p_dict)
+
+    return p
+
+# only one tree provided
+# root, params, sample already loaded
+
+s_dict = {0:{}, 1:{}}
+p_dict = {0:{}, 1:{}}
 
 root_decendants = root.descendants
 root_distr = root.cat
 
-ssum = 0
-for i in range(0, 3) :
-    ssum += root_distr[0][i] * rec(root, i)
+# Calculate p(beta|X)
+ssum = 0.0
+for i in range(0, len(root_distr[0])) :
+    s = root_distr[0][i] * rec(root, i, s_dict)
+    s_dict[i][root] = s
+    ssum += s
 
+# Print it
 val = ssum
-print("Value: " + str(val))
+print("Value" + str(val))
+
+####################################################################################################
+#
+# Posterior
+#
+####################################################################################################
+
+# root
+denom = 0
+for k in range(0, 2) :
+    s = s_dict[k][root]
+    posterior = s * root_distr[0][k]
+    denom += posterior
+    p_dict[k][root] = posterior
+
+# normalize root posterior 
+for j in range(0, 2) :
+    p_dict[j][root] = p_dict[j][root] / denom
+
+
+for dec in root.descendants :
+    # Calculate values for the rest of the nodes
+    compute_posterior(dec, s_dict, p_dict)
+
+for dec in root.descendants :
+    # Normalize values for the rest of the nodes
+    normalize_posterior(dec, p_dict)
+
+for dec in root.descendants :
+    for j in range(0, 2) :
+        print(p_dict[j][dec])
+
+# Assemble the product posterior
+p1 = assemble_posterior(root, 0, p_dict)
+p2 = assemble_posterior(root, 1, p_dict)
+
+print("p1, p2")
+print(p1)
+print(p2)
+
+denom = p1 + p2
+p1 = p1/denom
+p2 = p2/denom
+print(p1)
+print(p2)
+
+sample = np.random.multinomial(100, [p1, p2])
+print(sample)
