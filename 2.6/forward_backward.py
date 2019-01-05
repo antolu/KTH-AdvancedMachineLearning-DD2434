29ed7085a0f541321bcae7c1154cc727ceed0e87
Anton Ly <anton96lu@gmail.com>
2019-01-05 18:03:25 +0100

    Diverging beta :D

--- 2.6/forward_backward.py
+++ 2.6/forward_backward.py
@@ -1,15 +1,20 @@
 import numpy as np
+import math as m
+import scipy.stats
 
+def normal_distr(mu, sigma, x) :
+    return 1/(m.sqrt(2*m.pi*sigma)) * m.exp(((x - mu)**2)/(2*sigma))
 
-def get_transition(r1, m1, r2):
+Atr = np.array([[1/4, 3/4], [3/4, 1/4]])
+
+def get_transition(state1, m1, state2):
     # calculate A((r1, m1), (r2, m1+1)) (for test purpose we set below)
-    A = 0.3
-    return A
+    return Atr[state1][state2]
 
 
-def get_emission(r, o):
+def get_emission(state, m, emission, mus, beta, players):
     # calculate O(m, o) (for test purpose we set below)
-    O = 0.5
+    O = normal_distr(mus[players[0]-1][(state,m)] + mus[players[1]-1][(state,m)], beta, emission)
     return O
 
 
@@ -21,7 +26,7 @@ def get_init():
     return pi, R
 
 
-def forward(get_init, get_transition, get_emission, observations):
+def forward(observations, mus, beta, players):
     pi, R = get_init()
     M = len(observations)
     alpha = np.zeros((M, R))
@@ -29,7 +34,7 @@ def forward(get_init, get_transition, get_emission, observations):
     # base case
     O = []
     for r in range(R):
-        O.append(get_emission(r, observations[0]))
+        O.append(get_emission(r, 0, observations[0], mus, beta, players))
     alpha[0, :] = pi * O[:]
 
     # recursive case
@@ -37,14 +42,14 @@ def forward(get_init, get_transition, get_emission, observations):
         for r2 in range(R):
             for r1 in range(R):
                 transition = get_transition(r1, m, r2)
-                emission = get_emission(r2, observations[m])
+                emission = get_emission(r2, m, observations[m], mus, beta, players)
                 alpha[m, r2] += alpha[m - 1, r1] * transition * emission
 
 
     return (alpha, np.sum(alpha[M - 1, :]))
 
 
-def backward(get_init, get_transition, get_emission, observations):
+def backward(observations, mus, bet, players):
     pi, R = get_init()
     M = len(observations)
     beta = np.zeros((M, R))
@@ -57,20 +62,20 @@ def backward(get_init, get_transition, get_emission, observations):
         for r1 in range(R):
             for r2 in range(R):
                 transition = get_transition(r1, m, r2)
-                emission = get_emission(r2, observations[m + 1])
+                emission = get_emission(r2, m, observations[m + 1], mus, bet, players)
                 beta[m, r1] += beta[m + 1, r2] * transition * emission
 
     O = []
     for r in range(R):
-        O.append(get_emission(r, observations[0]))
+        O.append(get_emission(r, m, observations[0], mus, bet, players))
 
     return beta, np.sum(pi * O[:] * beta[0, :])
 
 
 
 # test examples
-print(forward(get_init, get_transition, get_emission, [0, 0, 1, 1, 1, 1]))
-print(backward(get_init, get_transition, get_emission, [0, 0, 1, 1, 1, 1]))
+# print(forward(get_init, get_transition, get_emission, [0, 0, 1, 1, 1, 1]))
+# print(backward(get_init, get_transition, get_emission, [0, 0, 1, 1, 1, 1]))
 
 
 
--- 2.6/main.py
+++ 2.6/main.py
@@ -1,4 +1,35 @@
 import pickle
+import numpy as np
+from forward_backward import *
+
+def forwardbackward(observations, mus, beta) :
+    players = list(observations.keys())
+
+    gammas = {}
+
+    for player in players : 
+        sequences = observations[player]
+        
+        rounds = list(sequences.keys())
+        gamma_per_round = {}
+
+        for round_ in rounds :
+            alpha, norm = forward(sequences[round_], mus, beta, player)
+            bet, temp = backward(sequences[round_], mus, beta, player)
+            gamma_per_round[round_-1] = alpha * bet
+        
+        gammas[player] = gamma_per_round
+    
+    return gammas
+
+def get_gamma_sum(gammas, i, m, n, np) :
+    gamma_sum = 0
+
+    for r in range(R) :
+        gammas[(n, np)]
+        gamma_sum += gammas[(n, np)][r][m, i]
+
+    return gamma_sum
 
 def load_obj(name ):
     with open('./' + name + '.pkl', 'rb') as f:
@@ -9,15 +40,88 @@ R = 10
 N = 20
 M = 30
 
+# Initialize parameters
+mus = {}
+beta = 20
+gammas = {}
+
+for n in range(N) :
+    mus[n] = {}
+    for i in range(2) :
+        for m in range(M) :
+            mus[n][(i,m)] = 15+i*5
+
+# Sort and count players
 players = list(sequence_outputs.keys())
 
-# print(players)
-# 190 players
+player_list = {}
+player_list_inv = {}
+for i in range(N) :
+    player_list[i] = list()
+    player_list_inv[i] = list()
 
-sequence = sequence_outputs[players[0]]
+for player in players : 
+    player_list[player[0] - 1].append(player[1])
+for player in players : 
+    player_list_inv[player[1] - 1].append(player[0])
 
-# sequence : dict
-# key: r
-# M long sequence for each r
+############################################################
+# Iterate EM
+############################################################
 
-print(len(sequence[1]))
+for j in range(100) :
+    old_mus = mus
+    oldbeta = beta
+    print(j)
+    gammas = forwardbackward(sequence_outputs, mus, beta)
+
+    nom = 0
+    denom = 0
+
+    for i in range(2) :
+        for m in range(M) :
+            A = np.zeros((N, N))
+            b = np.zeros(N)
+            for n in range(N) : 
+                gamma_n_np = np.zeros((N))
+
+                for npr in player_list[n] :
+                    gamma_sum = get_gamma_sum(gammas, i, m, n+1, npr)
+                    gamma_n_np[npr-1] = gamma_sum
+                    
+                    A[n, npr-1] = gamma_sum * mus[npr-1][(i,m)]
+
+                    # Calculate b for n(n, npr)
+                    for r in range(R) :
+                        b[n] += gammas[(n+1, npr)][r][m, i] * sequence_outputs[(n+1, npr)][r+1][m]
+                    
+                for npr in player_list_inv[n] :
+                    gamma_sum = get_gamma_sum(gammas, i, m, npr, n+1)
+                    gamma_n_np[npr-1] = gamma_sum
+                    
+                    A[n, npr-1] = gamma_sum * mus[npr-1][(i,m)]
+
+                    # Calculate b for n(n, npr)
+                    for r in range(R) :
+                        b[n] += gammas[(npr, n+1)][r][m, i] * sequence_outputs[(npr, n+1)][r+1][m]
+                
+                A[n, n] = np.sum(gamma_n_np) * mus[n][(i,m)]
+
+            solved_mus = np.linalg.solve(A, b)
+
+            # Assign mus
+            for n in range(N) :
+                mus[n][(i,m)] = solved_mus[n]
+
+        # Do some convergence checking
+        for m in range(M) :
+            for n in range(N) :
+                for npr in range(N) :
+                    for r in range(R) :
+                        if (n, npr) in gammas :
+                            nom += gammas[(n+1, npr+1)][r][m, i] * (sequence_outputs[(n+1, npr+1)][r+1][m] - mus[n][(i,m)] - mus[npr][(i,m)])**2 
+                            denom += gammas[(n+1, npr+1)][r][m, i]
+    
+    beta = nom/(2*denom)
+
+print(mus)
